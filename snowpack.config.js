a224e43a76073a02c4d00315c7fa678b298fde41
kjj6198 <kjj6198@gmail.com>
2020-11-04 01:54:35 +0900

    add webpack bundle

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   mount: {
-    public: '/',
-    src: '/_dist_',
+    public: "/",
+    src: "/_dist_",
   },
   plugins: [
-    '@snowpack/plugin-svelte',
-    '@snowpack/plugin-dotenv',
+    "@snowpack/plugin-svelte",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-webpack",
   ],
   install: [
     /* ... */
